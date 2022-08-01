c4da90743c8d9099b39c5bf11e9a4a77fe9ed10e
KPaulina <41211513+KPaulina@users.noreply.github.com>
2022-08-01 23:10:33 +0200

    feat: models and admin

--- list_of_read_books/admin.py
+++ list_of_read_books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserListBooks, ListAuthors
+
+
+class UserListBooksAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category']
+    readonly_fields = ['user', 'timestamp']
+
+
+admin.site.register(UserListBooks)
+admin.site.register(ListAuthors)
+
--- list_of_read_books/models.py
+++ list_of_read_books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class UserListBooks(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    review = models.TimeField(null=True)
+    category = models.CharField(max_length=50) #read #reading #want to read
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class ListAuthors(models.Model):
+    name = models.CharField(max_length=150)
+    surname = models.CharField(max_length=150)
+    name_surname = models.ForeignKey(UserListBooks, on_delete=models.CASCADE)
+    books = models.TextField(null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
