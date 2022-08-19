30a0a73d093673ec365c4b2064397d68e7203bc1
KPaulina <41211513+KPaulina@users.noreply.github.com>
2022-08-19 18:17:51 +0200

    fix: deny accedd to edit and delete books of another user

--- list_of_read_books/views.py
+++ list_of_read_books/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserListBooks, ListAuthors
 from django.urls import reverse_lazy
-from .forms import BooksAuthorsForm, BooksForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
@@ -34,30 +32,21 @@ class BookUpdateView(LoginRequiredMixin, UpdateView):
     fields = ['title', 'description', 'status']
     success_url = reverse_lazy('list_of_read_books:list')
 
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionError
+        return super().dispatch(request, *args, **kwargs)
+
 
 class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = UserListBooks
-    success_url = reverse_lazy ('list_of_read_books:list')
+    success_url = reverse_lazy('list_of_read_books:list')
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionError
+        return super().dispatch(request, *args, **kwargs)
 
 
-# @login_required
-# def books_update_view(request, id=None):
-#     obj = get_object_or_404(UserListBooks, id=id, user=request.user)
-#     book_form = BooksForm(request.POST or None, instance=obj)
-#     author_form = BooksAuthorsForm(request.POST or None)
-#     # Formset = modelformset_factory(Model, from=ModelForm, extra=0)
-#
-#     context = {
-#         "form": book_form,
-#         "author_form": author_form,
-#         "object": obj
-#     }
-#     if all([book_form.is_valid(), author_form.is_valid()]):
-#         book_form = book_form.save(commit=False)
-#         book_form.save()
-#         author_form = author_form.save(commit=False)
-#         author_form.save()
-#         obj.user = request.user
-#         obj.save()
-#         context['message'] = 'Book updated'
-#     return render(request, "books/create-update.html", context)
